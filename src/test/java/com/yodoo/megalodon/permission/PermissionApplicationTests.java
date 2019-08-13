fc205d1c513b965c09ebbe0c675ebd314b375c10
luojinjun <luojinjun@yodoo.net.cn>
2019-08-13 09:16:17 +0800

    修改测试代码

--- src/test/java/com/yodoo/megalodon/permission/PermissionApplicationTests.java
+++ src/test/java/com/yodoo/megalodon/permission/PermissionApplicationTests.java
@@ -1,21 +1,16 @@
 package com.yodoo.megalodon.permission;
 
-import com.yodoo.megalodon.permission.api.UserManagerApi;
-import com.yodoo.megalodon.permission.dto.UserGroupDto;
-import com.yodoo.megalodon.permission.entity.UserGroup;
-import com.yodoo.megalodon.permission.entity.UserGroupDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PermissionApplicationTests {
 
-    @Autowired
-    private UserManagerApi userManagerApi;
+    @Test
+    public void contextLoads() {
+    }
+
 }
