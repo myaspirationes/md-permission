4f30d4655fcbb45bcee1922bcd5220d45830caa8
luojinjun <luojinjun@yodoo.net.cn>
2019-08-15 10:45:16 +0800

    添加测试

--- src/test/java/com/yodoo/megalodon/permission/PermissionApplicationTests.java
+++ src/test/java/com/yodoo/megalodon/permission/PermissionApplicationTests.java
@@ -1,7 +1,18 @@
 package com.yodoo.megalodon.permission;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {PermissionConfig.class })
-public class PermissionApplicationTests {
+import com.yodoo.megalodon.permission.config.PermissionConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PermissionConfig.class })
+public class PermissionTests {
+
+
+    @Test
+    public void test(){
+        System.out.println("========");
+    }
 }
